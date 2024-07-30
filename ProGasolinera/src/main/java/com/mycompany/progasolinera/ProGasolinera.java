bc7210ea8694867043eb0752d9967bb8e119ab60
LeninQuilo4 <lenin.quilo471@ist17dejulio.edu.ec>
2024-07-29 20:39:59 -0500

    proyecto gasolinera

---
+++ ProGasolinera/src/main/java/com/mycompany/progasolinera/ProGasolinera.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.progasolinera;
+
+/**
+ *
+ * @author Lenin
+ */
+public class ProGasolinera {
+
+    public static void main(String[] args) {
+        inicio login = new inicio ();
+        login.setVisible(true);
+    }
+}
